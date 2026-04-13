ea0fe545a31b522bf5cf3c19fa0795875c65b719
hoehoeabi <0203jyh00@naver.com>
2026-04-13 18:07:23 +0900

    feat : 의존성 주입 및 공통변수들 선언

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -1,2 +1,39 @@
+import controller.ArticleController;
+import repository.ArticleRepository;
+import service.ArticleService;
+
+import java.util.Scanner;
+
 public class Container {
+    private static Scanner sc;
+    private static ArticleRepository articleRepository;
+    private static ArticleService articleService;
+    private static ArticleController articleController;
+
+    public static void init() {
+        sc = new Scanner(System.in);
+        articleRepository = new ArticleRepository();
+        articleService = new ArticleService(articleRepository);
+        articleController = new ArticleController(articleService,sc);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            scClose();
+            System.out.println("\n스캐너는 닫았으니 메모리 누수는 걱정 말라");
+        }));
+    }
+
+    public static void scClose() {
+        if (sc != null) {
+            sc.close();
+        }
+    }
+
+    public static Scanner getScanner(){
+        return sc;
+    }
+
+    public static ArticleController getArticleController() {
+        return articleController;
+    }
+
 }
