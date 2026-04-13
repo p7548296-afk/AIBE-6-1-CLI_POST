a6022b287b9c886ea7552509d3b30ff126c5c433
hoehoeabi <0203jyh00@naver.com>
2026-04-13 18:08:17 +0900

    feat : 컨트롤러가 일을 던지면 레포지토리에서 필요한 작업 수행함

--- src/main/java/service/ArticleService.java
+++ src/main/java/service/ArticleService.java
@@ -1,4 +1,35 @@
 package service;
 
+import domain.Article;
+import repository.ArticleRepository;
+
+import java.util.List;
+
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public int writeArticle(String title, String content) {
+        return articleRepository.write(title, content);
+    }
+
+    public List<Article> getListArticles() {
+        return articleRepository.findAll();
+    }
+
+    public Article findById(int id) {
+        return articleRepository.findById(id);
+    }
+
+    public void updateArticle(Article article, String title, String content) {
+        articleRepository.update(article, title, content);
+    }
+
+    public boolean deleteArticle(int id) {
+        return articleRepository.deleteById(id);
+    }
+
 }
