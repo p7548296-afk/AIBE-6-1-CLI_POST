fef5e9c04993ae2a04ab51cd5c990703c5e02194
hoehoeabi <0203jyh00@naver.com>
2026-04-14 17:03:55 +0900

    test : Rq 테스트

--- src/test/java/util/RqTest.java
+++ src/test/java/util/RqTest.java
@@ -29,4 +29,25 @@ class RqTest {
         Rq rq = new Rq("detail abc");
         assertThat(rq.getId()).isEqualTo(0);
     }
+    @Test
+    @DisplayName("공백이 섞여있어도 경로를 잘 추출해야 한다")
+    void t4() {
+        Rq rq = new Rq("  detail  1  ");
+        assertThat(rq.getActionPath()).isEqualTo("detail");
+        assertThat(rq.getId()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("숫자가 너무 크면 id는 0이 되어야 한다 (int 범위 초과)")
+    void t5() {
+        Rq rq = new Rq("detail 99999999999999999");
+        assertThat(rq.getId()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("명령어 없이 공백만 들어왔을 때 에러가 나지 않아야 한다")
+    void t6() {
+        Rq rq = new Rq("   ");
+        assertThat(rq.getActionPath()).isNotNull();
+    }
 }
